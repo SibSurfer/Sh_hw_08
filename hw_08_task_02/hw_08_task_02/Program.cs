690a66cb20b1980983973159aec20aece0670d27
Ilia Bondarenko <iluharider2002@gmail.com>
2024-03-20 09:18:57 +0300

    hw_08_task_02

---
+++ hw_08_task_02/hw_08_task_02/Program.cs
@@ -0,0 +1,49 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        object sync = new object();
+        bool isThread1Turn = true;
+
+        var thread1 = new Thread(() =>
+        {
+            lock (sync)
+            {
+                for (short i = 0; i < 10; i++)
+                {
+                    while (!isThread1Turn)
+                    {
+                        Monitor.Wait(sync);
+                    }
+                    Console.WriteLine("T1: " + i);
+                    isThread1Turn = false;
+                    Monitor.Pulse(sync);
+                }
+            }
+        });
+
+        var thread2 = new Thread(() =>
+        {
+            lock (sync)
+            {
+                for (short i = 0; i < 10; i++)
+                {
+                    while (isThread1Turn)
+                    {
+                        Monitor.Wait(sync);
+                    }
+                    Console.WriteLine("T2: " + i);
+                    isThread1Turn = true;
+                    Monitor.Pulse(sync);
+                }
+            }
+        });
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+    }
+}
